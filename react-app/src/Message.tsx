0453ee67200598808da2e1cfc8e85fc6d41f97cb
Gustavo Rocha <gurocha05@gmail.com>
2025-02-27 18:45:25 -0300

    Update Message.tsx

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,9 +1,12 @@
 function Message() {
+// const name = ""
 const name = "Gustavo"
 
-    return (
-        <h1>Hello {name}</h1>
-    )
+    if (name)
+        return <h1>hello {name}!</h1>
+
+    return <h1>Hello World!</h1>
+  
 }
 
 export default Message
